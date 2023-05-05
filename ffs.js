f64b0523a9aac1c95a1257fc453dc1de1e7431c9
xaukael <37848032+xaukael@users.noreply.github.com>
2023-05-05 16:56:54 -0500

    Add files via upload

--- ffs.js
+++ ffs.js
@@ -874,7 +874,7 @@ ffs.configure = async function(name) {
             [...Object.keys(game.settings.get('core', 'fonts')), ...CONFIG.fontFamilies].reduce((a,f)=>a+=`<option value="${f}" style="font-family: ${f};">${f}</option>`,`<select class="fontFamily" style="width:100%">`) + `</select>`
           }
           ${[...Array(10)].map((x,i)=>(i+1)*100).reduce((a,w)=>a+=`<option value="${w}" style="font-weight: ${w};">${w}</option>`,`<select class="fontWeight" style="width:100%">`)+`</select>`}
-          ${[...Array(50)].map((x,i)=>(i+1)).reduce((a,w)=>a+=`<option value="${w}">${w}px</option>`,`<select class="fontSize" style="width:100%">`)+`</select>`}
+          ${[...Array(50)].map((x,i)=>Math.max((i+2)*2*2)).reduce((a,w)=>a+=`<option value="${w}">${w}px</option>`,`<select class="fontSize" style="width:100%">`)+`</select>`}
           <input class="fontColor" type="color" value="" style="border:unset; padding: 0; width: 100%">
           
           `,
